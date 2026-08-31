eb27cc3643bb3ef94e324d8ff74c7b99da7ed409
R0GUEEE <r0gueee@users.noreply.github.com>
2026-08-31 02:50:58 -0400

    deps: build qemu-system-x86_64-unsigned launcher stub (darwin install requires it)

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -391,7 +391,7 @@ build_qemu_tcti () {
         -Dc_args="$(meson_array "$QEMU_CFLAGS")" \
         -Dc_link_args="$(meson_array "$QEMU_LDFLAGS")"
     echo "${GREEN}Building QEMU...${NC}"
-    ninja libqemu-x86_64-softmmu.dylib trace/trace-events-all
+    ninja libqemu-x86_64-softmmu.dylib trace/trace-events-all qemu-system-x86_64-unsigned
     echo "${GREEN}Installing QEMU...${NC}"
     meson install --no-rebuild
     cd "$pwd"
